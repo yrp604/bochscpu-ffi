142456e4cfd03872085ce08344b8c370456a18fc
yrp <yrp604@protonmail.com>
2020-02-19 00:24:07 -0800

    build both C and C++ bindings

--- build.rs
+++ build.rs
@@ -6,7 +6,14 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::Builder::new()
-        .with_crate(crate_dir)
+        .with_crate(&crate_dir)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("bochscpu.hpp");
+
+    cbindgen::Builder::new()
+        .with_crate(&crate_dir)
+        .with_language(cbindgen::Language::C)
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("bochscpu.h");
