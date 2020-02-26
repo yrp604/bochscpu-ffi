36826574e2797875604a4aa7428efc9e2987207b
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2020-02-26 15:58:56 -0800

    Needs an usize not an u32.

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,4 @@
 #[no_mangle]
-pub extern "C" fn bochscpu_log_set_level(level: u32) {
+pub extern "C" fn bochscpu_log_set_level(level: usize) {
     stderrlog::new().verbosity(level).init().unwrap();
 }
