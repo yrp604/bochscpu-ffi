fb1f4daa3c291800de37ec6e558adc14fc900518
Theodor <ftri2001@gmail.com>
2023-05-25 13:49:28 +0300

    Extended instr/opcode support:
    - Added support for Bochs' disassemble module
    - Added support for bxInstruction's dst/src functions

--- src/instr.rs
+++ src/instr.rs
@@ -1,6 +1,7 @@
-use std::ffi::c_void;
+use std::ffi::{c_char, c_void};
 
 use bochscpu::opcode::*;
+use bochscpu::Address;
 
 #[allow(non_camel_case_types)]
 pub type bochscpu_instr_t = *const c_void;
@@ -23,4 +24,27 @@ pub unsafe extern "C" fn bochscpu_instr_imm32(p: bochscpu_instr_t) -> u32 {
 #[no_mangle]
 pub unsafe extern "C" fn bochscpu_instr_imm64(p: bochscpu_instr_t) -> u64 {
     instr_imm64(p)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bochscpu_instr_src(p: bochscpu_instr_t) -> u32 {
+    instr_src(p)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bochscpu_instr_dst(p: bochscpu_instr_t) -> u32 {
+    instr_dst(p)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn bochscpu_opcode_disasm(
+    is32: u32,
+    is64: u32,
+    cs_base: *mut Address,
+    ip: *mut Address,
+    instr_bytes: *mut u8,
+    distbuf: *const c_char,
+    disasm_style: DisasmStyle,
+) -> u32 {
+    opcode_disasm_wrapper(is32, is64, cs_base, ip, instr_bytes, distbuf, disasm_style)
 }
